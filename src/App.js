5d88f581ef996ff020f0583a28e940bdd9b178d9
vitorhd10 <vitorhd10@gmail.com>
2021-09-22 16:32:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+function App () {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <form>
+    <input type="text" name="produto" placeholder="Produto"/>
+    <input type="value" name="quantidade" placeholder="Quantidade"/>
+    <input type="value" name="preco" placeholder="Preço"/>
+    <button type="submit">Salvar</button>
+  </form>
+  )
 }
 
 export default App;
