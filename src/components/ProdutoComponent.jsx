73b5b5a826c2cb654ae30cf1e62ddc31e5e70eb3
Vitor Hugo <Vitorhd10@gmail.com>
2021-10-06 22:17:52 -0300

    06/10/2021

--- src/components/ProdutoComponent.jsx
+++ src/components/ProdutoComponent.jsx
@@ -7,6 +7,10 @@ class ProdutoComponent extends React.Component {
     super(props);
 
     this.state = {
+      id: this.props.match.params.id,
+      nome: "",
+      quantidade: "",
+      valor: "",
       produtos: [{ id: 5, nome: "Computador I5", quantidade: 9, valor: 3200 }],
       modalIsOpen: false,
     };
@@ -16,6 +20,19 @@ class ProdutoComponent extends React.Component {
     this.addProduto = this.addProduto.bind(this);
     this.editProduto = this.editProduto.bind(this);
     this.deleteProduto = this.deleteProduto.bind(this);
+    this.changeProdutoHandler = this.changeProdutoHandler.bind(this);
+    this.changeQuantidadeHandler = this.changeQuantidadeHandler.bind(this);
+  }
+
+  componentDidMount() {
+    ProdutoServices.getProdutoById(this.state.id).then((res) => {
+      let produto = res.data;
+      this.setState({
+        nome: produto.nome,
+        quantidade: produto.quantidade,
+        valor: produto.valor,
+      });
+    });
   }
 
   deleteProduto(id) {
@@ -82,10 +99,10 @@ class ProdutoComponent extends React.Component {
               <table className="table table-striped table-haver ">
                 <thead>
                   <tr>
-                    <th>Id</th>
-                    <th>Produto</th>
-                    <th>Quantidade</th>
-                    <th>Valor</th>
+                    <th className="text-center">Id</th>
+                    <th className="text-center">Produto</th>
+                    <th className="text-center">Quantidade</th>
+                    <th className="text-center">Valor</th>
                     <th></th>
                   </tr>
                 </thead>
@@ -93,58 +110,91 @@ class ProdutoComponent extends React.Component {
                   {this.state.produtos.map((produto) => {
                     return (
                       <tr key={produto.id}>
-                        <th>{produto.id}</th>
-                        <td>{produto.nome}</td>
+                        <th className="text-center">{produto.id}</th>
+                        <td className="text-center">{produto.nome}</td>
                         <td>{produto.quantidade}</td>
                         <td>{produto.valor}</td>
 
                         <th>
-                          <button
-                            onClick={() =>
-                              window.confirm(
-                                `Voce deseja excluir este item com Id ${produto.id}?`
-                              ) && this.deleteProduto(produto.id)
-                            }
-                            className="btn btn-danger"
-                          >
-                            Excluir
-                          </button>
-                          <div className="App">
+                          <div>
                             <button
-                              style={{ marginLeft: "30px" }}
                               onClick={() =>
-                                this.setState({ modalIsOpen: true })
+                                window.confirm(
+                                  `Voce deseja excluir este item com Id ${produto.id}?`
+                                ) && this.deleteProduto(produto.id)
                               }
-                              className="btn btn-secondary"
+                              className="btn btn-danger"
                             >
-                              Editar
+                              Excluir
                             </button>
-                            <Modal isOpen={this.state.modalIsOpen}>
-                              <h2>Editar produto</h2>
-                              <input
-                                placeholder="Nome do Produto"
-                                name="nome"
-                                className="form-control"
-                                value={this.state.nome}
-                                onChange={this.changeProdutoHandler}
-                              />
-                              <div>
+                          </div>
+                        </th>
+                        <th>
+                          <button
+                            onClick={() => this.setState({ modalIsOpen: true })}
+                            className="btn btn-secondary"
+                          >
+                            Editar
+                          </button>
+                        </th>
+                        <div className="card col-md-6 offset-md-3 offset-md-3">
+                          <div className="App">
+                            <Modal
+                              isOpen={this.state.modalIsOpen}
+                              className="card col-md-4 offset-md-4 offset-md-4"
+                            >
+                              <h2 className="text-center">Editar produto</h2>
+                              <form>
+                                <div className="form-group">
+                                  <label> Produto: </label>
+                                  <input
+                                    placeholder="Nome do Produto"
+                                    name="nome"
+                                    className="form-control"
+                                    value={this.state.nome}
+                                    onChange={this.changeProdutoHandler}
+                                  />
+                                </div>
+                                <div className="form-group">
+                                  <label> Quantidade: </label>
+                                  <input
+                                    placeholder="Quantidade"
+                                    name="quantidade"
+                                    className="form-control"
+                                    value={this.state.quantidade}
+                                    onChange={this.changeQuantidadeHandler}
+                                  />
+                                </div>
+                                <div className="form-group">
+                                  <label> Valor: </label>
+                                  <input
+                                    placeholder="Valor"
+                                    name="valor"
+                                    className="form-control"
+                                    value={this.state.valor}
+                                    onChange={this.changeValorHandler}
+                                  />
+                                  <br />
+                                </div>
                                 <button
-                                  onClick={() => this.editProduto(produto.id)}
+                                  className="btn btn-primary"
+                                  onClick={() => this.editProduto}
                                 >
                                   Salvar
                                 </button>
                                 <button
+                                  style={{ marginLeft: "20px" }}
+                                  className="btn btn-danger"
                                   onClick={() =>
                                     this.setState({ modalIsOpen: false })
                                   }
                                 >
                                   Cancelar
                                 </button>
-                              </div>
+                              </form>
                             </Modal>
                           </div>
-                        </th>
+                        </div>
                       </tr>
                     );
                   })}
